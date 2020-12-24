5f95385b6c9c6795e340db4357388c335d8daecd
Geoffrey McGill <geoff@object.net>
2020-12-24 13:47:59 -0700

    Update to latest Retype build

--- docs/resources/js/config.js
+++ docs/resources/js/config.js
@@ -1,1 +1,58 @@
-var __DOCS_CONFIG__ = {"base":"/","version":"1.0.0","searchPlaceholder":"Search docs","searchHotkeys":["/"],"sidebarFilterPlaceholder":"Filter","toolbarFilterPlaceholder":"Filter","showSidebarFilter":true,"filterNotFoundMsg":"No member names found containing the query \"{query}\"","maxHistoryItems":15,"scrollOffset":20,"access":[{"value":"public","label":"Public"},{"value":"private","label":"Private"},{"value":"protected","label":"Protected"},{"value":"readonly","label":"Read only"}],"toolbarLinks":[{"id":"configs","label":"Configs"},{"id":"properties","label":"Properties","shortLabel":"Props"},{"id":"methods","label":"Methods"},{"id":"events","label":"Events"}],"topnav":[],"sidebar":[{"n":"/","l":"Retype CLI"}]};
+var __DOCS_CONFIG__ = {
+  "base": "/",
+  "version": "1.0.0",
+  "searchPlaceholder": "Search docs",
+  "searchHotkeys": [
+    "/"
+  ],
+  "sidebarFilterPlaceholder": "Filter",
+  "toolbarFilterPlaceholder": "Filter",
+  "showSidebarFilter": true,
+  "filterNotFoundMsg": "No member names found containing the query \"{query}\"",
+  "maxHistoryItems": 15,
+  "scrollOffset": 20,
+  "access": [
+    {
+      "value": "public",
+      "label": "Public"
+    },
+    {
+      "value": "private",
+      "label": "Private"
+    },
+    {
+      "value": "protected",
+      "label": "Protected"
+    },
+    {
+      "value": "readonly",
+      "label": "Read only"
+    }
+  ],
+  "toolbarLinks": [
+    {
+      "id": "configs",
+      "label": "Configs"
+    },
+    {
+      "id": "properties",
+      "label": "Properties",
+      "shortLabel": "Props"
+    },
+    {
+      "id": "methods",
+      "label": "Methods"
+    },
+    {
+      "id": "events",
+      "label": "Events"
+    }
+  ],
+  "topnav": [],
+  "sidebar": [
+    {
+      "n": "/",
+      "l": "Retype CLI"
+    }
+  ]
+};
