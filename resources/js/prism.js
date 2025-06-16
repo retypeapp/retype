13e8804794c6e8c8151235eced22da8b3da96260
Retype GitHub Action <retype@object.net>
2025-06-16 18:13:34 +0000

    Refreshes Retype-generated documentation.
    Process triggered by geoffreymcgill.

--- resources/js/prism.js
+++ resources/js/prism.js
@@ -1,4 +1,4 @@
-/*! Retype v3.10.0 | retype.com | Copyright 2025. Object.NET, Inc. All rights reserved. */
+/*! Retype v3.10.1 | retype.com | Copyright 2025. Object.NET, Inc. All rights reserved. */
 
 window.Prism=window.Prism||{manual:true};
 /*! For license information please see prism.js.LICENSE.txt */
